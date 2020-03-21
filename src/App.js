35358d05aa218004638b7b10bf2ecac6ade93738
Leonard Lin <gwokae@gmail.com>
2020-03-21 18:21:07 +0800

    * app skeleton and style * add style component * add prettier configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  > * {
+    padding: 0.6em;
+    box-sizing: border-box;
+  }
+  ul.notes {
+    margin: 0;
+    list-style: none;
+  }
+
+  ul.notes,
+  div.viewer {
+    display: inline-block;
+    width: 50%;
+    vertical-align: top;
+  }
+`;
 function App() {
   return (
-    <div className="App">
-      <h1>ProtonNote</h1>
-    </div>
+    <Container className='App'>
+      <div className='actions'>
+        <button>
+          <span role='img' aria-label='new note'>
+            ➕
+          </span>
+          New note
+        </button>
+      </div>
+      <ul className='notes'>
+        <li>note1</li>
+        <li>note2</li>
+        <li>note3</li>
+      </ul>
+      <div className='viewer'>view panel</div>
+    </Container>
   );
 }
 
