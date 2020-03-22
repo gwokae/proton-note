cc2b51772e7e46229cb99cc4105a306f1dcc6f61
Leonard Lin <gwokae@gmail.com>
2020-03-22 15:07:37 +0800

    * react-markdown integration * integrate with decrypt utils

--- src/components/NoteViewer.js
+++ src/components/NoteViewer.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { decrypt } from '../utils';
 
 function NodeList({ note }) {
+  const [content, setContent] = useState();
+  useEffect(() => {
+    setContent('## Decrypting');
+    decrypt(note.content).then((decrypted) => setContent(decrypted));
+  }, [note]);
   return (
     <div className='viewer'>
-      {note.title}
-      {note.content}
+      <h1>{note.title}</h1>
+      <section>
+        <ReactMarkdown source={content} />
+      </section>
+      <footer></footer>
     </div>
   );
 }
